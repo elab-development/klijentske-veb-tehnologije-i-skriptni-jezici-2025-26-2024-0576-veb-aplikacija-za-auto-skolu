2dd91d434331bd05e5ce8da82538438322560b3e
BUGI-123 <stefanbugarski05@gmail.com>
2026-08-08 20:11:37 +0200

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Lessons from './pages/Lessons';
+import Profile from './pages/Profile';
+import ScheduleLesson from './pages/ScheduleLesson';
+
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/lessons' element={<Lessons />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/schedule' element={<ScheduleLesson />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/ScheduleLesson.tsx
@@ -0,0 +1,5 @@
+const ScheduleLesson = () => {
+  return <div>Schedule Lesson</div>;
+};
+
+export default ScheduleLesson;
