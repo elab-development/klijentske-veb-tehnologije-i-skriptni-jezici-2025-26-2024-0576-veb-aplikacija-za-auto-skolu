59d366f9bf2a1ad257cb89076d5b319dd2f9719b
BUGI-123 <stefanbugarski05@gmail.com>
2026-08-14 20:08:42 +0200

    Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,15 @@
+import BenefitsSection from '../components/home/BenefitsSection';
+import HeroSection from '../components/home/HeroSection';
+import PopularCoursesSection from '../components/home/PopularCoursesSection';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className='bg-[#f7f8fa] font-["DM_Sans",sans-serif] text-[#0b1d3a]'>
+      <HeroSection />
+      <BenefitsSection />
+      <PopularCoursesSection />
+    </main>
+  );
 };
 
 export default Home;
